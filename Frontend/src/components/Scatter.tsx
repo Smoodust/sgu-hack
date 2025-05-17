c72e5ad07c66941d685eecdd7a0b01642b63bb4e
iliyagoga <100951861+iliyagoga@users.noreply.github.com>
2025-05-17 13:32:21 +0400

    afl;al

---
+++ Frontend/src/components/Scatter.tsx
@@ -0,0 +1,11 @@
+import { PieChartElement } from "../styled/Chart.styled";
+import { Subtitle } from "../styled/Base.styled";
+import { observer } from "mobx-react-lite";
+
+const Scatter = observer(()=>{
+    return     <PieChartElement>
+          <Subtitle>Error logs counts</Subtitle>
+          </PieChartElement>
+})
+
+export default Scatter;
